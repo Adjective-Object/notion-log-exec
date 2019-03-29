680b8e69f707eba4d6068f4cde3aeb135821e5a2
Travis CI User <travis@example.org>
2019-03-29 02:51:28 +0000

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [str(ir.req) for ir in reqs]
 
 setuptools.setup(
     name="notion_log_exec",
-    version="0.2.2",
+    version="0.2.3",
     author="Maxwell Huang-Hobbs",
     author_email="mhuan13+pypy@gmail.com",
     description="Runs a command and reports the result back to a notion collection",
